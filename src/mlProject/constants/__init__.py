286e962913184c7a6afe018f07886afe4266468a
Saurav Kumar <sauravk8400@gmail.com>
2025-07-19 15:40:22 -0700

    for j notebook

--- src/mlProject/constants/__init__.py
+++ src/mlProject/constants/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yml")
-PARAMS_FILE_PATH = Path("params.yml")
-SCHEMA_FILE_PATH = Path("schema.yml")
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
